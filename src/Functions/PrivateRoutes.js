1c7b2473ac746da712a079084f95bf2881bd790f
cipherbeast03 <88836313+cipherbeast03@users.noreply.github.com>
2023-07-21 18:52:49 +0530

    1 file modified

--- src/Functions/PrivateRoutes.js
+++ src/Functions/PrivateRoutes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRoute = () => {
-  let auth = { token: true };
+  let auth = { token: false };
   return auth.token ? <Outlet /> : <Navigate to="/login" />;
 };
 
