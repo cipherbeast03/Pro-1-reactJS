b578da7a1d89139b7e8efa03b2271f9f286a5cd7
cipherbeast03 <88836313+cipherbeast03@users.noreply.github.com>
2023-07-26 01:57:48 +0530

    2 files modified

--- server/backend.js
+++ server/backend.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 5000;
+const port = 3010;
 
 app.use(bodyParser.json());
 
--- src/Contact/contact.js
+++ src/Contact/contact.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './contact.css';
 
 const Contact = () => {
